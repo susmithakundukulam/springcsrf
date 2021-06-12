7f887d2a26fc7387beb93ed3c25e71f2752f9927
Susmitha <susmithakundukulam@gmail.com>
2021-06-12 14:07:11 +0530

    authentication from DB
    user and role in same table

--- src/main/java/com/ocp/demo/config/MySecurityConfig.java
+++ src/main/java/com/ocp/demo/config/MySecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ocp.demo.config;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +12,48 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource secDataSource;
+	//-----------------------------------------------------------------------------------------
+	//Incase if you need role and login to be in different table say users,authorities
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception { //add our users for in memory authentication
+	 * 
+	 * //above spring version 5... withDefaultPasswordEncoder is deprecated . so
+	 * ignore warning //Rough Code for hard coding login credentials
+	 * 
+	 * UserBuilder users = User.withDefaultPasswordEncoder();
+	 * auth.inMemoryAuthentication()
+	 * .withUser(users.username("susmitha").password("test123").roles("EMPLOYEE"))
+	 * .withUser(users.username("tom").password("test123").roles("EMPLOYEE",
+	 * "MANAGER"))
+	 * .withUser(users.username("christo").password("test123").roles("ADMIN"));
+	 * 
+	 * 
+	 * //code for login credentials to get from database
+	 * System.out.println("************************secDataSource");
+	 * auth.jdbcAuthentication().dataSource(secDataSource);
+	 * 
+	 * }
+	 */
+	//-----------------------------------------------------------------------------------------
+	//Incase if you need role and login to be in one table
 
-	//Enabled CSRF TOKEN
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    http
-	 		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-	 			
-	}
-	
-	
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder()).dataSource(secDataSource)
+				.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		 .authoritiesByUsernameQuery("select username, role from users where username=?")
+		;
+	}  
+
 }
